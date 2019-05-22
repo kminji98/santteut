bfa26c67cffc6e54a5385815137e3b66ac5d90e4
woojju24 <woojju24@gmail.com>
2019-05-22 09:27:03 +0900

    이우주 - 로그인 메뉴, css 수정

--- common/lib/login_menu.php
+++ common/lib/login_menu.php
@@ -6,30 +6,59 @@
     <li><a href="#">국내산행</a></li>
     <li><a href="#">해외산행</a></li>
     <li><a href="#">날짜별 산행</a></li>
-  </ul><!-- end of nav_menu1 -->
+  </ul>
   </div>
   <div id="login_menu">
     <ul id="login_menu_ul" >
-    <?php
-    if(!isset($_SESSION['id']) || empty($_SESSION['id'])){
-      echo ('<li ><a href="http://'.$_SERVER['HTTP_HOST'].'/santteut/member/login/login.php" class="hov">로그인</a></li>');
-      echo ('<li><a href="http://'.$_SERVER['HTTP_HOST'].'/santteut/member/join/join_member.php" class="hov">회원가입</a></li>');
-      echo ('<li id="top_my" class="hov">커뮤니티<small>▼</small>
-        <div id="top_my_content">
-          <ul id="top_my_content_ul">
-            <li><a href="#">자유게시판</a></li>
-            <li><a href="#">명산정보</a></li>
-            <li><a href="#">공식산행후기</a></li>
-          </ul>
-        </div>
-      </li> ');
-    }else if(!($_SESSION['id']=="admin")){
-      echo (" {$_SESSION['id']} 님 환영합니다. ");
-      echo ('<li><a href="#">로그아웃</a></li> ');
-      echo ('<li><a href="#">My</a></li> ');
-      echo ('<li><a href="#">커뮤니티</a></li> ');
-    }
-    ?>
+      <?php
+
+      // 비회원일때
+      if(!isset($id)){
+         echo ('<li ><a href="http://'.$_SERVER['HTTP_HOST'].'/santteut/member/login/login.php" class="hov">로그인</a></li>');
+        echo ('<li><a href="http://'.$_SERVER['HTTP_HOST'].'/santteut/member/join/join_member.php" class="hov">회원가입</a></li>');
+        echo ('<li id="top_my" class="hov">커뮤니티<small>▼</small>
+          <div id="top_my_content">
+            <ul id="top_my_content_ul">
+              <li><a href="#">자유게시판</a></li>
+              <li><a href="#">명산정보</a></li>
+              <li><a href="#">공식산행후기</a></li>
+            </ul>
+          </div>
+        </li> ');
+
+        //관리자일때
+      }else if(!($_SESSION['id']=="admin")){
+        echo (" {$_SESSION['name']} 님 환영합니다. ");
+        echo ('<li><a href="#">로그아웃</a></li> ');
+        echo ('<li id="top_my" class="hov">관리자모드<small>▼</small>
+          <div id="top_my_content">
+            <ul id="top_my_content_ul">
+              <li><a href="#">상품등록</a></li>
+              <li><a href="#">예약/결제목록</a></li>
+              <li><a href="#">답변글관리</a></li>
+              <li><a href="#">매출관리</a></li>
+              <li><a href="#">회원관리</a></li>
+            </ul>
+          </div>
+        </li> ');
+
+        //회원일때
+      }else{
+        echo (" {$_SESSION['name']} 님 환영합니다. ");
+        echo ('<li><a href="#">로그아웃</a></li> ');
+        echo ('<li id="top_my" class="hov">My<small>▼</small>
+          <div id="top_my_content">
+            <ul id="top_my_content_ul">
+              <li><a href="#">예약/결제내역</a></li>
+              <li><a href="#">참여내역</a></li>
+              <li><a href="#">내글관리</a></li>
+              <li><a href="#">상담문의</a></li>
+              <li><a href="#">장바구니</a></li>
+            </ul>
+          </div>
+        </li> ');
+      }
+      ?>
   </ul>
   </div>
 </div>
