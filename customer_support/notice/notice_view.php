8070931a3102e13f775cfb4ecf5c66d9fa7f3f3a
woojju24 <woojju24@gmail.com>
2019-05-23 09:05:37 +0900

    이우주 - 공지사항 작업중 2 notice_view, download

--- customer_support/notice/notice_view.php
+++ customer_support/notice/notice_view.php
@@ -1,5 +1,69 @@
 <?php
 session_start();
+
+if(!isset($_SESSION['id']=="admin")){
+  echo "<script>alert('권한없음!');history.go(-1);</script>";
+  exit;
+}
+
+include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/db_connector.php";
+
+$num=$name=$title=$content=$regist_day=$hit="";
+
+// 왜?
+// $image_width="";
+
+// 페이지가 없으면 디폴트 페이지 1페이지
+if(empty($_GET['present_page'])){$present_page=1;}else{$present_page=$_GET['present_page'];}
+
+
+if(isset($_GET["num"]) && !empty($_GET["num"])){
+    $num = test_input($_GET["num"]);
+    $hit = test_input($_GET["hit"]);
+    $q_num = mysqli_real_escape_string($conn, $num);
+
+    $sql="UPDATE `notice` SET `hit`=$hit WHERE `num`=$q_num;";
+    $result = mysqli_query($conn,$sql);
+    if (!$result) {
+      die('Error: ' . mysqli_error($conn));
+    }
+
+    $sql="SELECT * from `notice` where num ='$q_num';";
+    $result = mysqli_query($conn,$sql);
+    if (!$result) {
+      die('Error: ' . mysqli_error($conn));
+    }
+    $row=mysqli_fetch_array($result);
+    $name=$row['name'];
+    $hit=$row['hit'];
+    $title= htmlspecialchars($row['title']);
+    $content= htmlspecialchars($row['content']);
+    $title=str_replace("\n", "<br>",$title);
+    $title=str_replace(" ", "&nbsp;",$title);
+    $content=str_replace("\n", "<br>",$content);
+    $content=str_replace(" ", "&nbsp;",$content);
+    $file_name=$row['file_name'];
+    $file_copied=$row['file_copied'];
+    $file_type=$row['file_type'];
+    $regist_day=$row['regist_day'];
+
+    // 이미지 연관부분
+    //숫자 0 " " '0' null 0.0   $a = array()
+    // if(!empty($file_copied)){
+    //   //이미지 정보를 가져오기 위한 함수 width, height, type
+    //   $image_info=getimagesize("./data/".$file_copied);
+    //   $image_width=$image_info[0];
+    //   $image_height=$image_info[1];
+    //   $image_type=$image_info[2];
+    //   if($image_width>400) $image_width = 400;
+    // }else{
+    //   $image_width=0;
+    //   $image_height=0;
+    //   $image_type="";
+    // }
+    mysqli_close($conn);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ko" dir="ltr">
@@ -10,7 +74,6 @@ session_start();
     <title>공지사항</title>
   </head>
   <body>
-    <!--로그인 회원가입 로그아웃-->
     <header>
       <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/login_menu.php";?>
       <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/mini_menu.php";?>
@@ -21,15 +84,36 @@ session_start();
       <table border="1">
         <tr>
           <th>작성자</th>
-          <td style="width:600px; text-align:center;">관리자</td>
+          <td style="width:600px; text-align:center;"><?$name?></td>
         </tr>
         <tr>
           <th>제목</th>
-          <td></td>
+          <td><?$title?></td>
         </tr>
+
+        <!--여기서 다시 수정-->
         <tr style="height:400px;">
           <th>내용</th>
-          <td></td>
+          <td>
+            <!--이미지면 보여줌-->
+            <?php
+              //1. 해당된 가입자이고, 파일이 있으면, 파일명, 파일사이즈, 실제위치 정보확인
+              if(!empty($_SESSION['id']) && !empty($file_copied)){
+                $file_path = "./data/".$file_copied;
+                $file_size = filesize($file_path);
+                //2. 업로드된 이름을 보여주고 [저장] 할것인지 선택한다.  -----------?무슨말
+
+                // /*/*/*/*/*/*/*/*/*/*수정하기
+                echo ("
+                  첨부파일 : $file_name &nbsp; [ $file_size Byte ]
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href='download.php?mode=download&num=$q_num'>저장</a><br><br>
+                ");
+              }
+            ?>
+            <!--다른내용 보여줌-->
+            <?=$content?>
+          </td>
         </tr>
         <tr>
           <th>파일</th>
@@ -37,14 +121,15 @@ session_start();
         </tr>
       </table>
     <div class="admin">
-      <button id="admin_write_btn" type="button" name="button">수정</button>
-      <button id="admin_write_btn" type="button" name="button">삭제</button>
-      <button id="admin_write_btn" type="button" name="button">목록</button>
-    </div>
 
+    <!-- /*/*/*/*/*/*/*/*/*/*/*/*/*/*나중에 원클릭수정하기 -->
+      <button id="admin_write_btn" type="button" name="button"><a href="./notice_form.php?mode=update&num=<?$num?>">수정</a></button>
+      <button id="admin_write_btn" type="button" name="button" onclick="check_delete<?$num?>">삭제</button>');
+      <button id="admin_write_btn" type="button" name="button"><a href="./notice_list.php?present_page=<?=$present_page?>">목록</a></button>
+    </div>
     </section>
     <br>
-<br>
+    <br>
     <footer>
       <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/footer.php";?>
     </footer>
