d49dd1d00cf7600d2861b1a74015156c3911dc71
kminji98 <kminji98@gmail.com>
2019-05-24 20:08:37 +0900

    김민지 - create_table_base 수정

--- common/lib/create_table_base.php
+++ common/lib/create_table_base.php
@@ -73,6 +73,31 @@ function create_table($conn, $table_name){
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
      ";
        break;
+      case 'official_review' :
+      $sql = "CREATE TABLE `official_review` (
+        `num` int(11) NOT NULL AUTO_INCREMENT,
+        `title` varchar(100) NOT NULL,
+        `content` text NOT NULL,
+        `regist_day` char(20) DEFAULT NULL,
+        `main_img` char(50) NOT NULL,
+        `file_name_0` char(40) DEFAULT NULL,
+        `file_copied_0` char(30) DEFAULT NULL,
+        `file_type_0` char(30) DEFAULT NULL,
+        `file_name_1` char(40) DEFAULT NULL,
+        `file_copied_1` char(30) DEFAULT NULL,
+        `file_type_1` char(30) DEFAULT NULL,
+        `file_name_2` char(40) DEFAULT NULL,
+        `file_copied_2` char(30) DEFAULT NULL,
+        `file_type_2` char(30) DEFAULT NULL,
+        `file_name_3` char(40) DEFAULT NULL,
+        `file_copied_3` char(30) DEFAULT NULL,
+        `file_type_3` char(30) DEFAULT NULL,
+        `file_name_4` char(40) DEFAULT NULL,
+        `file_copied_4` char(30) DEFAULT NULL,
+        `file_type_4` char(30) DEFAULT NULL,
+        PRIMARY KEY (`num`)
+      ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;";
+       break;
         default:
 
       echo "<script>alert('해당 테이블이름이 없습니다. ');</script>";
