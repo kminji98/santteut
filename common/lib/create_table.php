43336f90430d8825372e38358c9ed0ff30bec2ad
kminji98 <kminji98@gmail.com>
2019-05-24 20:17:12 +0900

    김민지 - 테이블 생성 수정

--- common/lib/create_table.php
+++ common/lib/create_table.php
@@ -4,5 +4,6 @@ include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/create_table_base.php";
 create_table($conn,'member');
 create_table($conn,'package');
 create_table($conn,'notice');
-create_table($conn,'qna');
+create_table($conn,'official_review');
+// create_table($conn,'qna');
  ?>
