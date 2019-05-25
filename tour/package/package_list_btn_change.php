114ce0efe02e03c3b96bd2bb78e0f9ab23f696de
kminji98 <kminji98@gmail.com>
2019-05-25 23:49:04 +0900

    김민지 - 버튼 클릭 액션 추가

---
+++ tour/package/package_list_btn_change.php
@@ -0,0 +1,9 @@
+<?php
+
+$order_condition = $_POST['order_condition'];
+$order_option = $_POST['order_option'];
+$order_btn = $_POST['order_btn'];
+
+echo '[{"order_condition":"'.$order_condition.'","order_option":"'.$order_option.'","order_btn":"'.$order_btn.'"}]';
+
+ ?>
