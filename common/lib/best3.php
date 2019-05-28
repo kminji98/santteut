60b7824a0426308472284f5b93a6fd88a17ecbe8
kminji98 <kminji98@gmail.com>
2019-05-28 09:21:43 +0900

    김민지 - best3/ calendar UI 수정

--- common/lib/best3.php
+++ common/lib/best3.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/css/best3.css">
 </head>
+<link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/css/best3.css?ver=3">
+<body>
 <?php
 define(SCALE, 3);
 
@@ -19,7 +20,6 @@ define(SCALE, 3);
       $divide_flag='';
         break;
     }
-
   }
   $best3_sql = "SELECT * ,sum(`r_adult`+`r_kid`+`r_baby`) from `reserve` join `package` on `reserve`.`r_code`=`package`.`p_code` $divide_flag group by `r_code` order by sum(`r_adult`+`r_kid`+`r_baby`) desc limit 3;";
   $best3_result=mysqli_query($conn,$best3_sql);
@@ -39,85 +39,36 @@ define(SCALE, 3);
     $day[$best3_record] = $yoil[date('w', strtotime($p_dp_date))];
     $day2[$best3_record] = $yoil[date('w', strtotime($p_arr_date2))];
     $p_pay[$best3_record]=number_format($p_pay[$best3_record]);
-  }
+
+ ?>
+ <div class="container" id="con<?=($best3_record+1)?>">
+   <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/lib/editor/data/<?=$p_main_img_copy1[$best3_record]?>" alt="Avatar" class="image">
+   <div class="content">
+     TOP<?=($best3_record+1)?>
+   </div>
+     <div class="overlay">
+       <div class="text">
+         <h3><?=$p_name[$best3_record]?></h3>
+       </div>
+       <b>
+         <?=$p_pay[$best3_record]?>
+       </b>
+       <br><br>
+       <div class="text1">
+         &nbsp; <?=$p_dp_date[$best3_record]?> (<?=$day[$best3_record]?>) ~
+         <?=$p_dp_time[$best3_record]?><?php echo $p_arr_date2[$best3_record]." "."(".$day2[$best3_record].")"?>
+       </div>
+       <br>
+       <div class="button1">
+         <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/tour/package/package_view.php?mode=<?=$p_code[$best3_record]?>"><button>상품보기></button></a>
+       </div>
+     </div>
+   </div>
+
+<?php
+} //end of for
  ?>
 
-<link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/css/best3.css?ver=2">
-<body>
-    <div class="container" id="con1">
-      <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/lib/editor/data/<?=$p_main_img_copy1[0]?>" alt="Avatar" class="image">
-          <div class="content">
-            TOP1
-          </div>
-          <div class="overlay">
-
-            <div class="text">
-              <h3><?=$p_name[0]?></h3>
-            </div>
-            <b>
-              <?=$p_pay[0]?>
-            </b>
-            <br><br>
-            <div class="text1">
-              &nbsp; <?=$p_dp_date[0]?> (<?=$day[0]?>) ~
-              <?=$p_dp_time[0]?><?php echo $p_arr_date2[0]." "."(".$day2[0].")"?>
-
-            </div>
-            <br>
-            <div class="button1">
-              <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/tour/package/package_view.php?mode=<?=$p_code[0]?>">상품보기></a>
-            </div>
-
-          </div>
-        </div>
-        <div class="container" id="con2">
-          <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/lib/editor/data/<?=$p_main_img_copy1[1]?>" alt="Avatar" class="image">
-
-              <div class="content">
-                TOP2
-              </div>
-              <div class="overlay">
-                <div class="text">
-                  <h3><?=$p_name[1]?></h3>
-                </div>
-                <b>
-                  <?=$p_pay[1]?>
-                </b>
-                <br><br>
-                <div class="text1">
-                  &nbsp; <?=$p_dp_date[1]?> (<?=$day[1]?>) ~
-                  <?=$p_dp_time[1]?><?php echo $p_arr_date2[1]." "."(".$day2[1].")"?>
-                </div>
-                <br>
-                <div class="button1">
-                  <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/tour/package/package_view.php?mode=<?=$p_code[1]?>">상품보기></a>
-                  <!-- <button type="button" name="button">상품보기></button> -->
-                </div>
-              </div>
-</div>
-    <div class="container" id="con3">
-      <img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/lib/editor/data/<?=$p_main_img_copy1[2]?>" alt="Avatar" class="image">
-          <div class="content">
-            TOP3
-          </div>
-          <div class="overlay">
-            <div class="text">
-              <h3><?=$p_name[2]?></h3>
-            </div>
-            <b>
-              <?=$p_pay[2]?>
-            </b>
-            <br><br>
-            <div class="text1">
-              &nbsp; <?=$p_dp_date[2]?> (<?=$day[2]?>) ~
-              <?=$p_dp_time[2]?><?php echo $p_arr_date2[2]." "."(".$day2[2].")"?>
-            </div>
-            <br>
-            <div class="button1">
-              <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/tour/package/package_view.php?mode=<?=$p_code[2]?>">상품보기></a>
-            </div>
-          </div>
-        </div>
 </body>
 
 </html>
