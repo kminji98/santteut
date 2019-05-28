6cce56ebb284b118f1715fed65854b1ea8210e53
woojju24 <woojju24@gmail.com>
2019-05-28 13:29:54 +0900

    이우주- 커뮤메뉴 수정

--- common/lib/commu_menu.php
+++ common/lib/commu_menu.php
@@ -2,6 +2,6 @@
 <br>
     <ul id="commu_menu">
       <li style="margin-left:43%;" id="free_mini"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/community/free/free_list.php">자유게시판</a></li>
-      <li id="infomation_mini"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/community/qna/qna_list.php">명산정보</a></li>
-      <li id="official_mini"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/community/notice/notice_list.php">공식산행후기</a></li>
+      <li id="infomation_mini"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/community/mt_information/mt_information_list.php">명산정보</a></li>
+      <li id="official_mini"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/community/official_review/official_review_list.php">공식산행후기</a></li>
     </ul>
