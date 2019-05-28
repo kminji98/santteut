41d961f6636d839bceabb9e105c31eece5319072
ms960719 <ms960719@naver.com>
2019-05-28 20:36:39 +0900

    조민수-관리자모드 메뉴 추가

--- tour/admin/admin_manage_package.php
+++ tour/admin/admin_manage_package.php
@@ -222,6 +222,12 @@ include $_SERVER['DOCUMENT_ROOT']."/santteut/tour/package/package_list_query.php
     }
     </script>
     <title>산뜻 :: 즐거운 산행</title>
+    <style media="screen">
+    .page_button_group button{border-radius: 3px; margin-bottom:3%; width: 35px; height: 35px; font-weight: bold; margin-right: 5px; cursor: pointer; border: 1px solid #464646; background-color: white;}
+    .page_button_group button:hover{background-color: #2F9D27; color: white; border-radius: 3px; border: 1px solid #2F9D27;}
+    .page_button_group{ position: relative; margin-top: auto; margin-left: auto;text-align:center; margin-left: 12%; width: 100%; height: auto;}
+    a{text-decoration: none;}
+    </style>
   </head>
 
   <body>
@@ -248,10 +254,10 @@ include $_SERVER['DOCUMENT_ROOT']."/santteut/tour/package/package_list_query.php
           </tr>
 
           <?php
+          define('ROW_SCALE', 5);
+          define('PAGE_SCALE', 10);
           $sql = "SELECT * FROM `package`;";
           $manage_result=mysqli_query($conn,$sql);
-
-
           $total_record=mysqli_num_rows($manage_result);
 
           if($total_record==0){
@@ -260,7 +266,32 @@ include $_SERVER['DOCUMENT_ROOT']."/santteut/tour/package/package_list_query.php
                 </script>";
           }
 
-          for ($i = 0; $i  < $total_record; $i++){
+          $total_pages=ceil($total_record/ROW_SCALE);
+
+
+          // 페이지가 없으면 디폴트 페이지 1페이지
+          $page=(empty($_GET['page']))?1:$_GET['page'];
+
+
+
+          // 현재 블럭의 시작 페이지 = (ceil(현재페이지/블럭당 페이지 제한 수)-1) * 블럭당 페이지 제한 수 +1
+          //[[  EX) 현재 페이지 5일 때 => ceil(5/3)-1 * 3  +1 =  (2-1)*3 +1 = 4 ]]
+          $start_page= (ceil($page / PAGE_SCALE ) -1 ) * PAGE_SCALE +1 ;
+
+
+          // 현재페이지 시작번호 계산함.
+          //[[  EX) 현재 페이지 1일 때 => (1 - 1)*10 -> 0   ]]
+          //[[  EX) 현재 페이지 5일 때 => (5 - 1)*10 -> 40  ]]
+          $start_record=($page -1) * ROW_SCALE;
+
+
+          // 현재 블럭 마지막 페이지
+          // 전체 페이지가 (시작 페이지+페이지 스케일) 보다 크거나 같으면 마지막 페이지는 (시작페이지 + 페이지 스케일) -1 / 아니면 전체페이지 수 .
+          //[[  EX) 현재 블럭 시작 페이지가 6/ 전체페이지 : 10 -> '10 >= (6+10)' 성립하지 않음 -> 10이 현재블럭의 가장 마지막 페이지 번호  ]]
+          $end_page= ($total_pages >= ($start_page + PAGE_SCALE)) ? $start_page + PAGE_SCALE-1 : $total_pages;
+
+          mysqli_data_seek($manage_result,$start_record);
+          for ($record = $start_record; $record  < $start_record+ROW_SCALE && $record<$total_record; $record++){
             $row=mysqli_fetch_array($manage_result);
             $p_code=$row['p_code'];
             $p_name=$row['p_name'];
@@ -306,7 +337,7 @@ include $_SERVER['DOCUMENT_ROOT']."/santteut/tour/package/package_list_query.php
               <output><?php echo $p_arr_date2." "."(".$day2.")".$p_arr_time; ?></output>
             </td>
             <td class="package_list_period_value"><?=$p_period?>일</td>
-            <td class="package_list_view_name_value"><a href="package_view.php?mode=<?=$p_code?>"><?=$p_name?></a></td>
+            <td class="package_list_view_name_value"><a href="admin_up_del_package.php?mode=<?=$p_code?>"><?=$p_name?></a></td>
             <td class="package_list_view_pay_value"><?=$p_pay?></td>
             <td>
               <output class="package_list_view_state_value" id="list_status"><?=$status?></output>
@@ -318,6 +349,92 @@ include $_SERVER['DOCUMENT_ROOT']."/santteut/tour/package/package_list_query.php
            ?>
         </table>
 
+    </div>
+    <div class="page_button_group">
+
+      <?php
+      //현재 블럭의 시작 페이지가 페이지 스케일 보다 클 때 -> 처음으로 버튼 생성 + 이전 블럭 존재
+      //[ex]  page가 9개 있고 현재 페이지가 6페이지인 경우  / 12345/ 6789     =>  <<(처음으로) <(이전) 6 7 8 9
+      if( $start_page > PAGE_SCALE ){
+
+        // echo( '<a href='package_list.php?page=1'> << </a>' );
+
+        // 이전 블럭 클릭 시 -> 현재 블럭의 시작 페이지 - 페이지 스케일
+        // 현재 6 page 인 경우 '<(이전블럭)' 클릭 -> $pre_page = 6-PAGE_SCALE  -> 1 페이지로 이동
+        $pre_block= $start_page - PAGE_SCALE;
+         if(isset($_GET['mode'])){
+           switch ($_GET['mode']) {
+             case 'search':
+              echo( '<a href="package_list.php?mode=search&find_option='.$find_option.'&find_input='.$find_input.'&page=1&$divide='.$_GET['divide'].'"><button type="button" name="button" title="처음으로"><<</button></a>' );
+              echo( '<a href="package_list.php?mode=search&find_option='.$find_option.'&find_input='.$find_input.'&page='.$pre_block.'&divide='.$divide.'"><button type="button" name="button" title="이전"><</button></a>' );
+              break;
+            case 'detail':
+              echo( '<button type="button" title="처음으로" onclick="detail_search_mv_page(1)"><<</button>' );
+              echo( '<button type="button" title="이전" onclick="detail_search_mv_page('.$pre_block.')"><</button>' );
+              break;
+            case 'order':
+              echo( '<button type="button" title="처음으로" onclick="order_mv_page(1)"><<</button>' );
+              echo( '<button type="button" title="이전" onclick="order_mv_page('.$pre_block.')"><</button>' );
+              break;
+            default:
+              break;
+          }
+         }else{
+           echo( '<a href="package_list.php?page=1&divide='.$divide.'"><button type="button" name="button" title="처음으로"><<</button></a>');
+           echo( '<a href="package_list.php?page='.$pre_block.'&divide='.$divide.'"><button type="button" name="button" title="이전"><</button></a>');
+         }
+      }
+
+
+      //현재 블럭에 해당하는 페이지 나열
+      for( $i = $start_page; $i <= $end_page; $i++ ){
+          //현재 블럭에 현재 페이지인 버튼
+          if ( $i == $page ){
+            echo( '<a href="#"><button type="button" name="button" style="background-color: #2F9D27; border: 1px solid #2F9D27; color: white;">'.$i.'</button></a>' );
+          }else if(isset($_GET['mode'])){
+            switch ($_GET['mode']) {
+              case 'search':
+                echo( '<a href="package_list.php?mode=search&find_option='.$find_option.'&find_input='.$find_input.'&page='.$i.'&divide='.$divide.'"><button type="button" name="button">'.$i.'</button></a>' );
+                break;
+              case 'detail':
+                echo( '<button type="button" onclick="detail_search_mv_page('.$i.')">'.$i.'</button>' );
+                break;
+              case 'order':
+                echo( '<button type="button" onclick="order_mv_page('.$i.')">'.$i.'</button>' );
+                break;
+              default:
+                break;
+            }
+          }else{
+            echo( '<a href="package_list.php?page='.$i.'&divide='.$divide.'"><button type="button" name="button">'.$i.'</button></a>' );
+          }
+      }
+
+      if( $total_pages > $end_page ){
+        $next_block= $start_page + PAGE_SCALE;
+        if(isset($_GET['mode'])){
+          switch ($_GET['mode']) {
+            case 'search':
+              echo( '<a href="package_list.php?mode=search&find_option='.$find_option.'&find_input='.$find_input.'&page='.$next_block.'&divide='.$divide.'"  title="다음"><button type="button" name="button">></button></a>' );
+              echo( '<a href="package_list.php?mode=search&find_option='.$find_option.'&find_input='.$find_input.'&page='.$total_pages.'&divide='.$divide.'" title="맨끝으로"><button type="button" name="button">>></button></a>' );
+              break;
+            case 'detail':
+              echo( '<button type="button" title="다음" onclick="detail_search_mv_page('.$next_block.')">></button>' );
+              echo( '<button type="button" title="맨끝으로" onclick="detail_search_mv_page('.$total_pages.')">>></button>' );
+              break;
+            case 'order':
+              echo( '<button type="button" title="다음" onclick="order_mv_page('.$next_block.')">></button>' );
+              echo( '<button type="button" title="맨끝으로" onclick="order_mv_page('.$total_pages.')">>></button>' );
+              break;
+            default:
+              break;
+          }
+        }else{
+          echo( '<a href="package_list.php?page='.$next_block.'&divide='.$divide.'"><button type="button" name="button" title="다음">></button></a>' );
+          echo( '<a href="package_list.php?page='.$total_pages.'&divide='.$divide.'"><button type="button" name="button" title="맨끝으로">>></button></a>' );
+        }
+      }
+      ?>
     </div>
     <br><br><br>
 
---
+++ tour/admin/admin_up_del_package.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+include '../../common/lib/db_connector.php';
+?>
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/css/login_menu.css">
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/tour/admin/css/admin_add_package.css?ver=1">
+    <title>산뜻 :: 즐거운 산행</title>
+
+    <script type="text/javascript">
+      function check_input(){
+        saveContent();
+      }
+    </script>
+
+  </head>
+
+  <body>
+    <!--로그인 회원가입 로그아웃-->
+    <div id="wrap">
+    <header>
+      <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/login_menu.php";?>
+    </header>
+
+    <div id="content">
+      <br><br>
+      <h2>패키지 수정.삭제</h2><br>
+        <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/editor/editor.php"; ?>
+    </div>
+    <footer>
+      <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/footer.php";?>
+    </footer>
+
+    </div>
+
+  </body>
+</html>
