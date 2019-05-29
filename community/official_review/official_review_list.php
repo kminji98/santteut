1ca9e4d2b464a38eb6d082ea79c715ace4d22b6a
woojju24 <woojju24@gmail.com>
2019-05-29 14:15:03 +0900

    이우주 - 공식산행정보 수정완료 줄정리 아직

--- community/official_review/official_review_list.php
+++ community/official_review/official_review_list.php
@@ -10,7 +10,7 @@ session_start();
 include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/db_connector.php";
 
 //1. 게시물수 정의
-define('ROW_SCALE', 9);
+define('ROW_SCALE', 3);
 define('PAGE_SCALE', 5);
 
 //2. 변수정의
@@ -62,8 +62,7 @@ $view_num = $total_record - $start_record;
 <html lang="ko" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>공식산행후기</title>
+    <title>명산정보</title>
     <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/community/official_review/css/official_review_list.css?ver=0">
     <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/css/login_menu.css">
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
@@ -73,36 +72,33 @@ $view_num = $total_record - $start_record;
         $("#official_mini").css("color","black");
       });
       </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
   <body>
     <header>
       <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/login_menu.php";?>
       <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/commu_menu.php";?>
-
     </header>
     <div class="community_section">
-      <section>
-        <div id="official_review">공식산행후기</div>
-      </section>
       <section>
         <form name="board_form" action="official_review_list.php?mode=search" method="post">
           <table>
             <!-- <input type="hidden" name="mode" value="search"> -->
-            <tr id="list_search">
-              <td id="list_search1">총 <?=$total_record?> 개의 게시물이 있습니다.</td>
-              <td id="list_search3">
+            <ul id="list_search">
+              <li id="list_search1">total <?=$total_record?></li>
+
+              <li id="list_search3">
                 <select name="find_option">
                   <option value="title">제목</option>
+                  <option value="title">내용</option>
                 </select>
-              </td>
-              <td id="list_search4"><input type="text" name="find_input"></td>
-              <td id="list_search5"><input type="submit" value="검색" style="width:50px; height:24px; background-color: #2F9D27; border: 1px solid #2F9D27; color: white;"></td>
-            </tr>
+                <input id="list_search4" type="text" name="find_input">
+                <input id="list_search5" type="submit" value="검색">
+              </li>
+            </ul>
           </table>
         </form>
       </section>
-      <section id="image_list" style="padding-top: 50px;">
+      <section>
         <?php
           $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
@@ -115,21 +111,27 @@ $view_num = $total_record - $start_record;
             $regist_day = $row['regist_day'];
             $main_img = $row['main_img'];
             ?>
-            <a href="./official_review_view.php?num=<?=$num?>" id="review_a">
               <div class="review">
+                <a href="./official_review_view.php?num=<?=$num?>" id="review_a">
               <div class="review_img"><img src="./img/<?=$main_img?>" width="350px" height="400px"></div>
               <div class="review_title"><b><?=$title?></b></div><br>
-              </div>
             </a>
+              </div>
             <?php
             }
             ?>
       </section>
     </div>
-    <div class="clear"></div>
-    <div style="padding-bottom: 40px;">
-      <a href="./official_review_form.php"><input type="button" style="width:60px; height:24px; background-color: #2F9D27; border: 1px solid #2F9D27; color: white; margin-left:1200px;" value="글쓰기"></a>
-    </div>
+
+    <?php
+    if(!empty($_SESSION['id'])){
+      echo '<a href="official_review_form.php"><button id="write_but" type="button" name="button">
+      글쓰기</button></a>';
+    }
+     ?>
+
+
+
     <div class="page_button_group">
       <?php
       //현재 블럭의 시작 페이지가 페이지 스케일 보다 클 때 -> 처음으로 버튼 생성 + 이전 블럭 존재
