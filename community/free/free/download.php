ff5d2fd7aa1d36e856b1380849f36aa087fa747c
woojju24 <woojju24@gmail.com>
2019-05-29 14:21:21 +0900

    이우주 - 프리게시판 수정

--- community/free/free/download.php
+++ community/free/free/download.php
@@ -1,50 +1,42 @@
-
 <?php
 session_start();
+if(!isset($_SESSION['userid'])){echo "<script>alert('권한없음!');history.go(-1);</script>";
+exit;}
+include $_SERVER['DOCUMENT_ROOT']."/free/db_connector.php";
+$row=$file_name_0=$file_copied_0=$file_type_0="";
 
-// isset함수는 불리언값을 리턴 true or false
-// 회원 or 비회원이면 권한없음, 관리자일때만 입장
-if(!isset($_SESSION['id'])){
-  echo "<script>alert('권한없음!');history.go(-1);</script>";
-  exit;
-}
-
-include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/db_connector.php";
-
-// 변수선언
-$row=$file_name=$file_copied=$file_type="";
-
-// 모드가 다운로드일때
-if(isset($_GET["mode"]) && $_GET["mode"]=="download"){
+if(isset($_GET["mode"])&&$_GET["mode"]=="download"){
     $num = test_input($_GET["num"]);
     $q_num = mysqli_real_escape_string($conn, $num);
-
-    //등록된사용자가 최근 입력한 공지사항게시판을 보여주기 위하여 num 찾아서 전달하기 위함이다.
-    $sql="SELECT * from `notice` where num ='$q_num';";
+    //등록된사용자가 최근 입력한 다운로드게시판을 보여주기 위하여 num 찾아서 전달하기 위함이다.
+    $sql="SELECT * from `free` where num ='$q_num';";
     $result = mysqli_query($conn,$sql);
     if (!$result) {
       alert_back('Error: 1' . mysqli_error($conn));
+      // die('Error: ' . mysqli_error($conn));
     }
     $row=mysqli_fetch_array($result);
     $file_name=$row['file_name'];
-    $file_copied=$row['file_copied'];
+    $destination=$row['destination'];
     $file_type=$row['file_type'];
     mysqli_close($conn);
 }
 
 // 1. 테이블에서 파일명이 있는지 점검
-if(empty($file_copied)){
+
+
+if(empty($destination)){
     alert_back(' 테이블에 파일명이 존재 하지 않습니다.!');
 }
-$file_path = "./data/$file_copied";
+
 
 //2. 서버에 Data영역에 실제 파일이 있는지 점검
-if(file_exists($file_path)){
-  $fp=fopen($file_path,"rb");  //$fp 파일 핸들값
+if(file_exists($destination)){
+  $fp=fopen($destination,"rb");  //$fp 파일 핸들값
   //지정된 파일타입일경우에는 모든 브라우저 프로토콜 규약이 되어있음.
   if($file_type){
     Header("Content-type: application/x-msdownload");
-    Header("Content-Length: ".filesize($file_path));
+    Header("Content-Length: ".filesize($destination));
     Header("Content-Disposition: attachment; filename=$file_name");
     Header("Content-Transfer-Encoding: binary");
     Header("Content-Description: File Transfer");
@@ -55,13 +47,13 @@ if(file_exists($file_path)){
     //타입이 알려지지 않았을때 익스플러러 프로토콜 통신방식
     if(eregi("(MSIE 5.0|MSIE 5.1|MSIE 5.5|MSIE 6.0)",$_SERVER['HTTP_USER_AGENT'])){
       Header("Content-type: application/octet-stream");
-      Header("Content-Length: ".filesize($file_path));
+      Header("Content-Length: ".filesize($destination));
       Header("Content-Disposition: attachment; filename=$file_name");
       Header("Content-Transfer-Encoding: binary");
       Header("Expires: 0");
     }else{
       Header("Content-type: file/unknown");
-      Header("Content-Length: ".filesize($file_path));
+      Header("Content-Length: ".filesize($destination));
       Header("Content-Disposition: attachment; filename=$file_name");
       Header("Content-Description: PHP3 Generated Data");
       Header("Expires: 0");
@@ -73,5 +65,6 @@ if(file_exists($file_path)){
 }else{
     alert_back(' 서버에 실제 파일이 존재 하지 않습니다.!');
 }
+
 
 ?>
