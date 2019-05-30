51def35820ddc2cefb5858b7a4cb05e228d9d5c0
dhrn0623 <dhrn0623@gmailcom>
2019-05-30 13:33:22 +0900

    오성훈 -footer 및 이용약관1,2,3 작업

--- common/lib/footer.php
+++ common/lib/footer.php
@@ -3,13 +3,11 @@
 + [DATE] 2019-05-27
 + [NAME] 이우주
 ================================================================= -->
-<!-- <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> -->
-<!-- <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet"> -->
-<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 <fieldset id="footer_wrap"style=" margin:0px; padding:0px; border:0px; color: white; height:auto; width: 100%; margin-bottom:0;">
-  <table style="margin-left: 5%;color:white; display:inline-block;;">
+  <table style="margin-left: 5%;color:white; display:inline-block;">
     <tr>
-      <td><h2 style=" font-family: 'Inconsolata', monospace; ">SANTTEUT mountaineering club </h2></td>
+      <td><h2 style="font-family: 'Montserrat', sans-serif;">SANTTEUT mountaineering club 산뜻산악회</h2></td>
     </tr>
     <tr>
       <td>서울특별시 성동구 무학로 2길 54 4-5층</td>
@@ -62,6 +60,26 @@
     </tr>
   </table>
 
+  <table style="margin:0; margin-left:5%; display:inline-block;">
+    <tr>
+      <td>
+        <p style="color: #999999; font-weight:bold;">약관</p>
+        <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common\lib\tos1.php">이용약관</a>
+      </td>
+    </tr>
+    <tr>
+      <td><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common\lib\tos2.php">개인정보 처리방침</a></td>
+    </tr>
+    <tr>
+      <td><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common\lib\tos3.php">여행약관</a></td>
+    </tr>
+    <tr>
+      <td>
+        &nbsp;
+      </td>
+    </tr>
+  </table>
+
 
 
 <br><br>
@@ -70,7 +88,7 @@
 <fieldset id="footer_wrap"style="background-color:#2E333D; margin:0px; padding:0px; border:0px; color: white; height:auto; width: 100%; margin-bottom:0;">
   <br style="height:1%;">
   <p style=" display: inline; margin-left: 5%;font-size:12px;">Copyright ⓒ 2019 santteut.com, All Rights Reserved.
-  <p style=" display: inline; margin-left: 40%;font-size:12px;">제휴문의: santteut@earthtory.com | 02-1234-5678</p>
+  <p style=" display: inline; margin-left: 30%;font-size:12px;">제휴문의: santteut@earthtory.com | 02-1234-5678</p>
   </p>
 
 </fieldset>
