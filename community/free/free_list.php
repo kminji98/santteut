3311014f2d937474e062a4d21c39cb6ba0ae4258
woojju24 <woojju24@gmail.com>
2019-05-31 00:18:51 +0900

    이우주 - 자유게시판 수정ㅡㅅㅡ

--- community/free/free_list.php
+++ community/free/free_list.php
@@ -4,9 +4,7 @@ session_start();
 include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/db_connector.php";
 define('SCALE', 10);
 //*****************************************************
-$sql=$result=$total_record=$total_page=$start="";
-$row="";
-$memo_id=$memo_num=$memo_date=$memo_nick=$memo_content="";
+$sql=$result=$total_record=$total_page=$start=$row="";
 $total_record=0;
 //*****************************************************
 if(isset($_GET["mode"])&&$_GET["mode"]=="search"){
@@ -33,14 +31,10 @@ $start=($page -1) * SCALE;
 $number = $total_record - $start;
 ?>
 <html lang="ko" dir="ltr">
-<style media="screen">
-table{  border-collapse: collapse;}
-  .t1 td{ border-bottom: 1px solid black;
-}
-</style>
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/css/login_menu.css">
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/community/free/css/free_list.css">
     <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/santteut/common/css/side_bar.css">
     <title></title>
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
@@ -58,30 +52,30 @@ table{  border-collapse: collapse;}
       <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/side_bar.php";?>
     </header>
 <section>
-
-
     <form class="" action="free_list.php?mode=search" method="post">
-<table style="width : 800px; margin : auto;">
-  <tr>
-    <th style="text-align : left;">total <?=$total_record?></th>
-    <td style="float : right;"><input type="submit" name="" value="검색"></td>
-    <td style="float : right;"><input type="text" name="" value=""></td>
-    <td style="float : right;"><select  name="find">
-      <option value="title">제목</option>
-      <option value="content">내용</option>
-      <option value="name">이름</option>
-      <option value="id">아이디</option>
-    </select></td>
-  </tr>
-</table>
+      <div class="">
+        <ul>
+          <li>total <?=$total_record?></li>
+          <li>
+            <select  name="find">
+              <option value="title">제목</option>
+              <option value="content">내용</option>
+              <option value="name">이름</option>
+              <option value="id">아이디</option>
+            </select>
+            <input type="text" name="" value="">
+            <input type="submit" name="" value="검색">
+          </li>
+        </ul>
+      </div>
     </form>
-    <table style="width : 800px; margin : auto;">
+    <table border="1">
       <tr class ="t1">
-        <td>번호</td>
-        <td style="width : 550px; text-align : center;">제목</td>
-        <td style="text-align : right;">글쓴이</td>
-        <td style="width : 100px;text-align : right;">등록일</td>
-        <td style="text-align : right;">조회</td>
+        <th>번호</th>
+        <th>제목</th>
+        <th>작성자</th>
+        <th>작성일</th>
+        <th>조회</th>
       </tr>
     <?php
     for ($i = $start; $i < $start+SCALE && $i<$total_record; $i++){
@@ -92,41 +86,39 @@ table{  border-collapse: collapse;}
       $name=$row['name'];
       $hit=$row['hit'];
       $title=$row['title'];
-      $date= substr($row['regist_day'],2,9);
+      $date= substr($row['regist_day'],0,10);
     ?>
-<tr class ="t1";style="border-bottom : 1px solid gray;">
-<td style="padding-left : 17px;"><?=$num?></td>
-<td style="width : 500px; padding-left: 15px;"><a href="./view.php?num=<?=$num?>&page=<?=$page?>&hit=<?=$hit+1?>"><?=$title?></a></td>
-<td style="text-align : center;"><?=$id?></td>
-<td style="text-align : right;"><?=$date?></td>
-<td style="padding-left : 30px;"><?=$hit?></td>
-</tr>
-<?php  }
-$number--;
-?>
-<tr>
-  <td colspan="2" style="text-align : center; padding-left : 30px;">
-    이전< &nbsp;&nbsp;&nbsp;&nbsp;
-  <?php
-    for ($i=1; $i <= $total_page ; $i++) {
-      if($page==$i){
-        echo "<b>&nbsp;$i&nbsp;</b>";
-      }else{
-        echo "<a href='./free_list.php?page=$i'>&nbsp;$i&nbsp;</a>";
-      }
-    }
-  ?>
-  &nbsp;&nbsp;&nbsp;&nbsp;> 다음
-</td>
-  <td></td>
-  <td style="padding-left : 35px;"><?php
-    if(!empty($_SESSION['id'])){
-    echo '<a href="write_edit_form.php"><input type="button" name="" value="글쓰기"></a>';
-    }
-  ?></td>
-  <td><a href="./free_list.php?page=<?=$page?>"><input type="button" name="" value="목록"> </a></td>
-</tr>
-    </table>
+    <tr class ="t1">
+      <td><?=$num?></td>
+      <td><a href="./view.php?num=<?=$num?>&page=<?=$page?>&hit=<?=$hit+1?>"><?=$title?></a></td>
+      <td><?=$name?></td>
+      <td><?=$date?></td>
+      <td><?=$hit?></td>
+    </tr>
+    <?php
+      $view_num--;
+     }//end of for
+    ?>
+  </table>
+        이전< &nbsp;&nbsp;&nbsp;&nbsp;
+      <?php
+        for ($i=1; $i <= $total_page ; $i++) {
+          if($page==$i){
+            echo "<b>&nbsp;$i&nbsp;</b>";
+          }else{
+            echo "<a href='./free_list.php?page=$i'>&nbsp;$i&nbsp;</a>";
+          }
+        }
+      ?>
+      &nbsp;&nbsp;&nbsp;&nbsp;> 다음
+
+    <?php
+        if(!empty($_SESSION['id'])){
+        echo '<a href="write_edit_form.php"><input type="button" name="" value="글쓰기"></a>';
+        }
+      ?>
+      <a href="./free_list.php?page=<?=$page?>"><input type="button" name="" value="목록"> </a></td>
+
     </section>
     <footer>
       <?php include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/footer.php";?>
