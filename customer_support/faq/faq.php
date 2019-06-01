d62cfaf98d426083973dae06f70c3763b4a3fabc
woojju24 <woojju24@gmail.com>
2019-06-02 00:31:27 +0900

    이우주 - faq완료

--- customer_support/faq/faq.php
+++ customer_support/faq/faq.php
@@ -48,36 +48,36 @@ include $_SERVER['DOCUMENT_ROOT']."/santteut/common/lib/db_connector.php";
        <tr id="faq"><td id="faq_td1"></td><td ><br></td></tr>
        <tr><td></br></td><td></td></tr>
        <!-- slidetoggle 액션  -->
-       <tr><td id="li1">Q.&nbsp;등산 시 속도를 못 쫓아갈까바 걱정이 됩니다. 괜찮을까요?</td></tr>
+       <tr><td id="li1" style="cursor: pointer;">Q.&nbsp;등산 시 속도를 못 쫓아갈까바 걱정이 됩니다. 괜찮을까요?</td></tr>
        <tr id="li1_1" style="display:none;"><td>
           &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>사람들마다 각자 산행의 속도 차이가 있습니다.<br>
-          &nbsp;&nbsp;&nbsp;&nbsp;그래서 보통 선두 그룹과 후미 그룹으로 나뉘어 진행되는 경우가 많으며, 일정표상의 소요시간은 후미그룹을 기준으로 패키지정보에 기재되어 있습니다.<br>
-          &nbsp;&nbsp;&nbsp;&nbsp;최종 목적지에 도착할 때까지 다른 일행들 보다 너무 뒤쳐지지 않게 신경써서 이동 해 주시고, 등산 스틱을 사용 하시면 보행 시간 단축에 도움을 받으실 수 있습니다.</small></td></tr>
-       <tr><td id="li2">Q.&nbsp;패키지마다 인솔자가 계시나요?</td></tr>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;그래서 보통 선두 그룹과 후미 그룹으로 나뉘어 진행되는 경우가 많으며, 일정표상의 소요시간은 후미그룹을 기준으로 패키지정보에 기재되어 있습니다.<br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;최종 목적지에 도착할 때까지 다른 일행들 보다 너무 뒤쳐지지 않게 신경써서 이동 해 주시고, 등산 스틱을 사용 하시면 보행 시간 단축에 도움을 받으실 수 있습니다.</small></td></tr>
+       <tr><td id="li2" style="cursor: pointer;">Q.&nbsp;패키지마다 인솔자가 계시나요?</td></tr>
        <tr id="li2_1" style="display:none;"><td >
-        &nbsp;<small><b>A. </b>산악회 직원이 인솔자로 동행하게 됩니다.</small></td></tr>
-       <tr><td id="li3">Q.&nbsp;등산 중 화장실은 어떻게 사용 하나요?</td></tr>
+        &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>산악회 직원이 인솔자로 동행하게 됩니다.</small></td></tr>
+       <tr><td id="li3"  style="cursor: pointer;">Q.&nbsp;등산 중 화장실은 어떻게 사용 하나요?</td></tr>
        <tr id="li3_1" style="display:none;"><td >
-        &nbsp;<small><b>A. </b>지역에 따라 상이 하나 보통 산장 또는 간이 화장실을 사용하며, 1회용 휴대 화장실을 사용해야 하는 경우도 있습니다.</small></td></tr>
-       <tr><td id="li4">Q.&nbsp;신용 불량자도 해외여행을 할 수 있나요?</td></tr>
+        &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>지역에 따라 상이 하나 보통 산장 또는 간이 화장실을 사용하며, 1회용 휴대 화장실을 사용해야 하는 경우도 있습니다.</small></td></tr>
+       <tr><td id="li4"  style="cursor: pointer;">Q.&nbsp;신용 불량자도 해외여행을 할 수 있나요?</td></tr>
        <tr id="li4_1" style="display:none;"><td >
-        &nbsp;<small><b>A. </b>신용 불량자는 금융 불량자입니다.<br>
-          &nbsp;&nbsp;&nbsp;&nbsp;그래서 단지 신용 불량자라고 해서 출국까지 금지 되는 것은 아닙니다.<br>
-          &nbsp;&nbsp;&nbsp;&nbsp;다만 신용 불량자가 되면서 거액으로 또는 민,형사 고소가 남아 있든지 하는 관계로 상대자가 출국금지를 요청 을 했을 때는 제외 사항입니다.<br>
-          &nbsp;&nbsp;&nbsp;&nbsp;참고로, 출국금지여부는 본인이 직접 가까운 출입국관리사무소를 방문하시면 확인이 가능함을 말씀 드립니다.</small></td></tr>
-       <tr><td id="li5">Q.&nbsp;회원탈퇴는 어떻게 하나요?</td></tr>
+        &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>신용 불량자는 금융 불량자입니다.<br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;그래서 단지 신용 불량자라고 해서 출국까지 금지 되는 것은 아닙니다.<br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;다만 신용 불량자가 되면서 거액으로 또는 민,형사 고소가 남아 있든지 하는 관계로 상대자가 출국금지를 요청 을 했을 때는 제외 사항입니다.<br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;참고로, 출국금지여부는 본인이 직접 가까운 출입국관리사무소를 방문하시면 확인이 가능함을 말씀 드립니다.</small></td></tr>
+       <tr><td id="li5" style="cursor: pointer;">Q.&nbsp;회원탈퇴는 어떻게 하나요?</td></tr>
        <tr id="li5_1" style="display:none;"><td >
-        &nbsp;<small><b>A. </b>상단의 오른쪽메뉴에서 MY > 정보수정에서 탈퇴를 하실수 있습니다.</small></td></tr>
-       <tr><td id="li6">Q.&nbsp;여행 경비는 얼마정도 가져가야 할까요?</td></tr>
+        &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>상단의 오른쪽메뉴에서 MY > 정보수정에서 탈퇴를 하실수 있습니다.</small></td></tr>
+       <tr><td id="li6" style="cursor: pointer;">Q.&nbsp;여행 경비는 얼마정도 가져가야 할까요?</td></tr>
        <tr id="li6_1" style="display:none;"><td >
-        &nbsp;<small><b>A. </b>상품정보에 포함 되어 있지 않은 사항을 확인 하시고 여행 일수 별로 개인 경비를 준비하시면 됩니다.<br>
-          &nbsp;&nbsp;&nbsp;&nbsp;자유시간이 없는 경우 선택관광이나 쇼핑은 할 수 없습니다.</small></td></tr>
-       <tr><td id="li7">Q.&nbsp;비회원도 사이트 이용이 가능한가요?</td></tr>
+        &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>상품정보에 포함 되어 있지 않은 사항을 확인 하시고 여행 일수 별로 개인 경비를 준비하시면 됩니다.<br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;자유시간이 없는 경우 선택관광이나 쇼핑은 할 수 없습니다.</small></td></tr>
+       <tr><td id="li7" style="cursor: pointer;">Q.&nbsp;비회원도 사이트 이용이 가능한가요?</td></tr>
        <tr id="li7_1" style="display:none;"><td >
-        &nbsp;<small><b>A. </b>자유게시판과 공지사항만 보기가 가능하며 그 외 기능은 회원가입후 이용가능하십니다.  </small></td></tr>
-       <tr><td id="li8">Q.&nbsp;숙식이랑 항공권은 개인부담인가요?</td></tr>
+        &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>자유게시판과 공지사항만 보기가 가능하며 그 외 기능은 회원가입후 이용가능하십니다.  </small></td></tr>
+       <tr><td id="li8" style="cursor: pointer;">Q.&nbsp;숙식이랑 항공권은 개인부담인가요?</td></tr>
        <tr id="li8_1" style="display:none;"><td >
-        &nbsp;<small><b>A. </b>모든 패키지가 숙식과 항공권이 포함되어있는 금액입니다.</small></td></tr>
+        &nbsp;<small>&nbsp;&nbsp;&nbsp;&nbsp;<b>A. </b>모든 패키지가 숙식과 항공권이 포함되어있는 금액입니다.</small></td></tr>
        <tr id="faq"><td id="faq_td1"></td><td width=""><br></td></tr>
      </table>
     </div>
